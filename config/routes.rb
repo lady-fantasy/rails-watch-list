f29e719bb25e479de1a0c8be5aced63854be782d
yasmine kordjazy <ykordjaz@gmail.com>
2021-11-11 16:42:02 +0100

    long day

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :lists, only: [ :index, :new, :create, :show ] do
+    resources :bookmarks, only: [ :new, :create, :destroy ]
+  end
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
